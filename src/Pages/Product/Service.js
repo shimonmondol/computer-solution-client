4c41e233d7b07f7c4f6b8adf8b15c4a88fb4e805
Shimon Mondol <shimonuap072@gmail.com>
2022-05-28 05:05:42 +0600

    tenth commit

--- src/Pages/Product/Service.js
+++ src/Pages/Product/Service.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Service = ({ service }) => {
@@ -8,14 +8,36 @@ const Service = ({ service }) => {
     const NavigatePService = id => {
         Navigate(`/service/${id}`);
     }
+    const [quantity, setQuantity] = useState(5);
+
+    const handleDecrement = () => {
+        if (quantity > 5) {
+            setQuantity(prevCount => prevCount - 1);
+        }
+    }
+    const handleIncrement = () => {
+        if (quantity < 30) {
+            setQuantity(prevCount => prevCount + 1);
+        }
+    }
+
     return (
         <div class="card w-80 bg-base-200 shadow-xl mt-12 mb-12">
             <div class="card-body items-center text-center">
                 <h2 class="card-title">{name}</h2>
                 <img className='w-60' src={img} alt="" />
-                <p>{minimum.length} products </p>
+                <p>{minimum.length} Products Available </p>
                 <p className='text-justify'>{description}</p>
                 <p>Price: {price} </p>
+                <p>Minimum : 5, Maximum: 30</p>
+
+                <div className='input-group'>
+                    <button type="button" onClick={handleDecrement} className='btn mt-6'>-</button>
+                    <div>{quantity}<input type="text" placeholder="" class="input w-48" /></div>
+
+                    <button type="button" onClick={handleIncrement} className='btn mt-6'>+</button>
+
+                </div>
                 <div class="card-actions">
                     <button onClick={() => NavigatePService(id)} className="btn btn-primary">Buy Now</button>
                 </div>
